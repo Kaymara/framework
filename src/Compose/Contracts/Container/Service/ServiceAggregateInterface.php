3b6cb4d303bd3ee9a3d53293012ce99971bd0f56
Derek Fulginiti <derek.fulginiti@gmail.com>
2020-01-30 21:32:56 -0500

    Add service aggregate interface definition

---
+++ src/Compose/Contracts/Container/Service/ServiceAggregateInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Compose\Contracts\Container\Service;
+
+interface ServiceAggregateInterface
+{
+    /**
+     * Add a service to the aggregate
+     *
+     * @param string $id
+     * @param ServiceInterface $service
+     * 
+     * @return ServiceInterface
+     */
+    public function add(string $id, $service) : ServiceInterface;
+
+    /**
+     * Does the given alias exist in the aggregate?
+     *
+     * @param string $id
+     * 
+     * @return boolean
+     */
+    public function has(string $id) : bool;
+
+    /**
+     * Does the given tag exist in the aggregate?
+     *
+     * @param string $tag
+     * @return boolean
+     */
+    public function tag(string $tag) : bool;
+
+    /**
+     * Resolve service from the aggregate
+     * 
+     * @param string $id
+     *
+     * @return ServiceInterface
+     */
+    public function make(string $id) : ServiceInterface;
+
+    /**
+     * Resolve services with given tag from the aggregate
+     *
+     * @param string $tag
+     * 
+     * @return mixed
+     */
+    public function makeTagged(string $tag) : ServiceAggregateInterface;
+}
