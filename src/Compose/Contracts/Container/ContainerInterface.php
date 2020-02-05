2b955487106dfa3afea63b18e554bd3d68019161
Derek Fulginiti <derek.fulginiti@gmail.com>
2020-02-04 19:36:45 -0500

    Change interface signatures

--- src/Compose/Contracts/Container/ContainerInterface.php
+++ src/Compose/Contracts/Container/ContainerInterface.php
@@ -11,33 +11,32 @@ interface ContainerInterface extends PsrContainerInterface
     /**
      * Add a service to the container
      * 
-     * @param string $id
-     * @param Closure|null $service
-     * @param bool $shared
+     * @param string $alias
+     * @param Closure|string|null $concrete
      * 
      * @return ServiceInterface
      */
-    public function add(string $id, $service = null, $shared = false) : ServiceInterface;
+    public function add(string $alias, $concrete = null) : ServiceInterface;
 
     /**
      * Add a singleton to the container
      * 
-     * @param string $id
-     * @param Closure|null $service
+     * @param string $alias
+     * @param Closure|string|null $concrete
      * 
      * @return ServiceInterface
      */
-    public function singleton(string $id, $service = null) : ServiceInterface;
+    public function singleton(string $alias, $concrete = null) : ServiceInterface;
 
     /**
      * Add an instance to the container
      * 
-     * @param string $id
+     * @param string $alias
      * @param mixed $instance
      * 
      * @return mixed
      */
-    public function instance(string $id, $instance);
+    public function instance(string $alias, $instance);
 
     /**
      * Clear the container of all services
