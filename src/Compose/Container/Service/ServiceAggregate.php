0ea4f97faef521b5e4ec43e788e489e84971561a
Derek Fulginiti <derek.fulginiti@gmail.com>
2020-02-04 19:16:26 -0500

    Create ServiceAggregate.php

---
+++ src/Compose/Container/Service/ServiceAggregate.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Compose\Container\Service;
+
+use Compose\Container\Exception\ServiceNotFoundException;
+use Compose\Container\Traits\ContainerAware;
+use Compose\Contrats\Container\Service\ServiceAggregateInterface;
+use Compose\Contracts\Container\Service\ServiceInterface;
+
+class ServiceAggregate implements ServiceAggregateInterface
+{
+    use ContainerAware;
+
+    /**
+     * Array of services
+     *
+     * @var ServiceInterface[]
+     */
+    protected $services = [];
+
+    /**
+     * Construct the aggregate
+     *
+     * @param ServiceInterface[] $services
+     */
+    public function __construct(array $services = []) {
+        $this->services = array_filter($services, fn($service) => $service instanceof ServiceInterface);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function add(string $alias, $service) {
+        if (! $service instanceof ServiceInterface) {
+            $service = new Service($alias, $service);
+        }
+
+        $this->services[] = $service;
+
+        return $service;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function exists(string $alias) {
+        foreach ($this->services as $service) {
+            if ($service->aliasedWith($alias)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function taggedWith(string $tag) {
+        foreach ($this->services as $service) {
+            if ($service->taggedWith($tag)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function service(string $alias) {
+        if (! $this->exists($alias)) {
+            throw new ServiceNotFoundException("Alias {$alias} is not known to a service."); 
+        }
+
+        foreach ($this->services as $service) {
+            if ($service->aliasedWith($alias)) {
+                return $service->setContainer($this->getContainer());
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function resolve(string $alias) {
+        return $this->service($alias)->resolve();
+    }
+
+    /**
+     * Resolve array of tagged services from the aggregate
+     *
+     * @param string $tag
+     * 
+     * @return mixed
+     */
+    public function resolveTagged(string $tag) {
+        $resolved = [];
+
+        foreach ($this->services as $service) {
+            if ($service->taggedWith($tag)) {
+                $resolved[] = $service->resolve();
+            }
+        }
+
+        return $resolved;
+    }
+}
