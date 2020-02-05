654cdd273abf4110a2d9c2370104f17b976dc1c8
Derek Fulginiti <derek.fulginiti@gmail.com>
2020-02-04 19:23:53 -0500

    Create SingletonAggregateInterface.php

---
+++ src/Compose/Contracts/Container/Service/SingletonAggregateInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Compose\Contracts\Container\Service;
+
+use Compose\Contracts\Container\ContainerAwareInterface;
+use Compose\Contracts\Container\Service\ServiceInterface;
+
+interface SingletonAggregateInterface extends ContainerAwareInterface
+{
+    /**
+     * Add a singleton to the aggregate
+     *
+     * @param string $alias
+     * @param mixed $service
+     * 
+     * @return ServiceInterface
+     */
+    public function add(string $alias, $service) : ServiceInterface;
+
+    /**
+     * Does a singleton with the given alias exist in the aggregate?
+     *
+     * @param string $alias
+     * 
+     * @return boolean
+     */
+    public function exists(string $alias) : bool;
+
+    /**
+     * Are there singletons with the given tag in the aggregate?
+     *
+     * @param string $tag
+     * 
+     * @return boolean
+     */
+    public function taggedWith(string $tag) : bool;
+
+    /**
+     * Retrieves singleton with given alias from the aggregate
+     *
+     * @param string $alias
+     * 
+     * @return ServiceInterface
+     */
+    public function service(string $alias) : ServiceInterface;
+
+    /**
+     * Resolve singleton with given alias from the aggregaate
+     *
+     * @param string $alias
+     * 
+     * @return mixed
+     */
+    public function make(string $alias);
+
+    /**
+     * Resolve array of tagged singletons from the aggregate
+     *
+     * @param string $tag
+     * 
+     * @return mixed
+     */
+    public function makeTagged(string $tag);
+}
