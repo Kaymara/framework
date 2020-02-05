d7b5238086c19f64c67c6e3150a16dc54b45f374
Derek Fulginiti <derek.fulginiti@gmail.com>
2020-02-04 19:23:44 -0500

    Create ServiceProviderAggregateInterface.php

---
+++ src/Compose/Contracts/Container/Service/ServiceProviderAggregateInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Compose\Contracts\Container\Service;
+
+use Compose\Contracts\Container\ContainerAwareInterface;
+use Compose\Container\Exception\ServiceProviderException;
+use IteratorAggregate;
+
+interface ServiceProviderAggregateInterface extends ContainerAwareInterface, IteratorAggregate
+{
+    /**
+     * Add a service provider to the aggregate
+     *
+     * @param string $alias
+     * @param string|ServiceProviderInterface $service
+     * 
+     * @return self
+     */
+    public function add($provider) : ServiceProviderAggregateInterface;
+
+    /**
+     * Is the given service provided by the aggregate?
+     *
+     * @param string $service
+     * 
+     * @return boolean
+     */
+    public function provides(string $service) : bool;
+
+    /**
+     * Registers all service providers in the aggregate
+     *
+     * @param string $provider
+     * 
+     * @return void
+     */
+    public function register();
+
+    /**
+     * Boots all service providers in the aggregate
+     *
+     * @param string $provider
+     * 
+     * @return void
+     */
+    public function boot();
+}
